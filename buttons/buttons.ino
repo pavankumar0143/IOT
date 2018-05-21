94f38d217eb6d677922aae8dbd2b5566f9121bca
pavan <pavankumar18131997@gmail.com>
2018-05-21 14:19:28 +0530

    modified if logic

--- buttons/buttons.ino
+++ buttons/buttons.ino
@@ -7,12 +7,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly: 
-
-  int button_value = digitalRead(PUSH1);
-  if (button_value == 0)
+  int button_value = 1 ;
+  button_value = digitalRead(PUSH1);
+  while (!button_value)
   {
     digitalWrite(BLUE_LED, HIGH);
     delay(1000);
+    button_value = digitalRead(PUSH1);
   }
   digitalWrite(BLUE_LED, LOW);
   delay(1000);
